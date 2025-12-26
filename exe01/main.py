b0eab8d494bfabc5324e846e97727f72abadaf9c
odiegosilva1 <odiegosilvadev@gmail.com>
2025-12-26 15:20:50 -0300

    add: exe01

---
+++ exe01/main.py
@@ -0,0 +1,10 @@
+# Interação com usuário
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    input('Você entrou no sistema')
+elif entrada == 'sair':
+    print('Vocẽ saiu do sistema')
+else:
+    print('Você não digitou nem entrar e nem sair.')    
+        
